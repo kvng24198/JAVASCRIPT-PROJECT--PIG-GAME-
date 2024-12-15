bfff09a485e5933ea8cd3f007b78f3cf3a7cfbe6
kvng24198 <kvngsamuel24198@gmail.com>
2024-12-15 05:41:35 -0800

    in progress

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 "use strict";
 const instruction = document.querySelector(".header");
-const instructionContent = document.querySelector(".instrution-content");
-const instructionBtn = document.querySelector("instruction-botton");
-
+const instructionContent = document.querySelector(".instruction-content");
+const instructionBtn = document.querySelector(".instruction-button");
+const overLay = document.querySelector(".overlay");
 // function open(){
 //     instructionContent.
 // }
 
 instruction.addEventListener("click", function () {
   instructionContent.classList.add("visible");
+  overLay.classList.add("result");
+});
+instructionBtn.addEventListener("click", function () {
+  instructionContent.classList.remove("visible ");
 });
