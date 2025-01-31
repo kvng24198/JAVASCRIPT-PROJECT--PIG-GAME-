5c03d89f494da2e5393defafad6981571de9dd5c
kvng24198 <kvngsamuel24198@gmail.com>
2025-01-31 12:11:31 -0800

    dice2 in progress

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ const digit_1 = document.querySelector(".digit-1");
 const score_0 = document.querySelector(".score-0");
 const score_1 = document.querySelector(".score-1");
 const set_score = document.querySelector(".set-score");
+const p_one = document.querySelector(".p-1");
+const p_two = document.querySelector(".p-2");
+const dice_numOne = document.querySelector(".dice-number-1");
+const dice_numTwo = document.querySelector(".dice-number-2");
 diceImg.style.visibility = "hidden";
 // // scores
 // digitOne1.textContent = "0";
@@ -59,17 +63,20 @@ overLay.addEventListener("click", function () {
 //dice roll
 rollBtn.addEventListener("click", function () {
   if (playing === true) {
-    //generate random number
+    // generate random number
     const dice = Math.trunc(Math.random() * 6) + 1;
     console.log(dice);
+    const dice1 = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice1);
     //display the dice
     diceImg.style.visibility = "visible";
-    diceNumber.src = `./img/dice-${dice}.jpg`;
+    dice_numTwo.src = `./img/dice-${dice}.jpg`;
+    dice_numOne.src = `./img/dice-${dice1}.jpg`;
 
     //check if dice value is 1
-    if (dice !== 1) {
+    if (dice !== 1 && dice1 !== 1) {
       //add the dice value to current score
-      currentScore += dice;
+      currentScore += dice1 + dice;
       console.log(currentScore, "current score");
       document.querySelector(`.digit-${activePlayer}`).textContent =
         currentScore;
@@ -132,11 +139,29 @@ function resetScore() {
   // switch the activeplayer to player-1
   activePlayer = 0;
 
+  // make the dice invisible when new game is clicked
+  diceImg.style.visibility = "hidden";
+  // reverse winner to active player when new game is clicked
+  document.querySelector(`.player-0-box`).classList.remove("winner-animation");
+  document.querySelector(`.player-1-box`).classList.remove("winner-animation");
+
+  /*   console.log(document.querySelector(`.p-1`));
+   */
+  /*  document.querySelector(`.p-1`).textContent = "player 1";
+  document.querySelector(`.p-2`).textContent = "player 2"; */
+  console.log(p_one.textContent);
+  document.querySelector(`.player-0-box`).innerHTML = p_one.textContent;
+  document.querySelector(`.player-1-box`).innerHTML = p_two.textContent;
+
+  document.querySelector(`.player-0-box`).innerHTML = activePlayer1.text;
   // add the active dot to player-1
   activePlayer1.classList.add("dot");
   activePlayer2.classList.remove("dot");
-  // make the dice invisible when new game is clicked
-  diceImg.style.visibility = "hidden";
+
+  //set playing to true
+  playing = true;
+  scores = [0, 0];
+  set_score.value = 0;
 }
 
 startBtn.addEventListener("click", resetScore);
